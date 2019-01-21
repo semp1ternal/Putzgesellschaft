f88363f214342fa629ffb963f832406b3f11dc07
semp1ternal <43723308+semp1ternal@users.noreply.github.com>
2019-01-21 22:51:30 +0100

    presence

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = client => {
     console.log(`Eingelog als ${client.user.tag}!`)
+    client.user.setPresence({ game: { name: 'Programmiert von Sҽɱριƚҽɾɳαʅ'}})
 
     const welcome = client.channels.find(c => c.name === '🎮game-list')
     const verify = client.channels.find(c => c.name === '📕infos-und-regelwerk')

619396d9a34fc6f97afc3124b38f6130234ea18a
semp1ternal <buettericholiver@gmail.com>
2019-03-22 00:23:16 +0100

    1.1.0
    Added Commands to Play!
    !resume doesent work because of queue

--- commands/play.js
+++ commands/play.js
@@ -1,35 +1,44 @@
 const yt = require('ytdl-core')
+const settings = require('../settings.json')
 
 exports.run = (client, message, args) => {
 	let queue = [];
 	let url = message.content.split(' ')[1];
-	
-	if (url != undefined || url != null){ // Check if exists
+
+	if (url != undefined || url != null) { // Check if exists
 		if (url == "" | !url.includes("youtube")) return message.reply("Not a youtube link."); // Check if valid link, if not reply with nonexistent or not valid
 		const voiceChannel = message.member.voiceChannel;
 		if (!voiceChannel) return message.reply("You aren't in any voice channels"); // Check if user in voice channel, if not reply with message
 		// Get info and add to queue
 		yt.getInfo(url, (err, info) => {
-			if(err) return message.channel.send('Invalid YouTube Link: ' + err);
+			if (err) return message.channel.send('Invalid YouTube Link: ' + err);
 			//queue.push(url)
 			voiceChannel.join()
 				.then(connection => {
-					const streamOptions = { seek: 0, volume: 1 };
-					const stream = yt(url, { filter : 'audioonly' });
+					const streamOptions = { seek: 0, volume: 1, passes: 4 };
+					const stream = yt(url, { filter: 'audioonly' });
 					const dispatcher = connection.playStream(stream, streamOptions);
+					let collector = message.channel.createCollector(m => m);
+					collector.on('collect', m => {
+						if (m.content.startsWith(settings.prefix + 'pause')) {
+							message.reply('paused').then(() => { dispatcher.pause(); });
+						} else if (m.content.startsWith(settings.prefix + 'resume')){
+							message.reply('resumed').then(() => {dispatcher.resume()})
+						}
+					})
 				})
-				.catch(console.error);
 		})
+
 	}
 }
-exports.help = {
-    name: 'play',
-    aliases: []
-}
+	exports.help = {
+		name: 'play',
+		aliases: []
+	}
 
 /* Hints
 Guild is the server
 Guild.ID is the server id (Used if the bot is running on multiple servers)
- 
+
 
 */

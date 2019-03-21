26d6278e1107f218a67386732c50f4351e2ae677
MyPC <edinlaw@live.com>
2019-03-21 21:34:08 +0100

    Fixed play.js

--- commands/play.js
+++ commands/play.js
@@ -1,28 +1,35 @@
 const yt = require('ytdl-core')
 
 exports.run = (client, message, args) => {
-
-    let queue = {};
-    if (queue[message.guild.id] === undefined) return message.reply(`Erst Songs hinzufügen`);
-    if (!message.guild.voiceConnection) return message.member.join(message).then(() => queue.push(args))
-    if (queue[message.guild.id].playing) return message.reply('Spielt bereits!')
-
-    let dispatcher;
-    queue[message.guild.id].playing = true
-
-    console.log(queue);
-    (function play(song) {
-        console.log(song)
-        if (song === undefined) return message.reply('Queue ist leer!').then(() => {
-            queue[message.guild.id].playing = false
-            message.member.voiceChannel.leave()
-        })
-        message.reply(`Spielt **${song.title}**, gewünscht von **${song.requester}**`)
-        dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }))
-
-    })
+	let queue = [];
+	let url = message.content.split(' ')[1];
+	
+	if (url != undefined || url != null){ // Check if exists
+		if (url == "" | !url.includes("youtube")) return message.reply("Not a youtube link."); // Check if valid link, if not reply with nonexistent or not valid
+		const voiceChannel = message.member.voiceChannel;
+		if (!voiceChannel) return message.reply("You aren't in any voice channels"); // Check if user in voice channel, if not reply with message
+		// Get info and add to queue
+		yt.getInfo(url, (err, info) => {
+			if(err) return message.channel.send('Invalid YouTube Link: ' + err);
+			//queue.push(url)
+			voiceChannel.join()
+				.then(connection => {
+					const streamOptions = { seek: 0, volume: 1 };
+					const stream = yt(url, { filter : 'audioonly' });
+					const dispatcher = connection.playStream(stream, streamOptions);
+				})
+				.catch(console.error);
+		})
+	}
 }
 exports.help = {
     name: 'play',
     aliases: []
 }
+
+/* Hints
+Guild is the server
+Guild.ID is the server id (Used if the bot is running on multiple servers)
+ 
+
+*/
